23fa520d523706ee65ea519eb238a29cf02feac7
Andre Heinecke <aheinecke@intevation.de>
2010-07-08 16:25:55 +0000

    Change dependency to the new gpg-dev package
    svn path=/branches/work/emerge/intevation-merge/; revision=1147556

--- portage/enterprise5/kdepimlibs-e5/kdepimlibs-e5-20100701.py
+++ portage/enterprise5/kdepimlibs-e5/kdepimlibs-e5-20100701.py
@@ -34,7 +34,7 @@ class subinfo(info.infoclass):
         self.hardDependencies['win32libs-sources/cyrus-sasl-src'] = 'default'
         self.hardDependencies['win32libs-sources/boost-src'] = 'default'
         self.hardDependencies['win32libs-sources/libical-src'] = 'default'
-        self.hardDependencies['win32libs-bin/gpgme'] = 'default'
+        self.hardDependencies['enterprise5/gpg-e5'] = 'default'
 
 from Package.CMakePackageBase import *
         
