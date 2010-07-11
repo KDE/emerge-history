9e87af316af76766aed59d3468e0d62d634fd7de
Sascha Lothar Teichmann <sascha.teichmann@intevation.de>
2010-07-11 13:41:03 +0000

    Added some docstrings to the 'emerge -k -j' builder.
    svn path=/branches/work/emerge/intevation-merge/; revision=1148664

--- bin/parallel.py
+++ bin/parallel.py
@@ -2,6 +2,41 @@
 # -*- coding: utf-8 -*-
 #
 # copyright 2010 by Intevation GmbH
+#
+"""Tool to use emerge in 'make -k' and 'make -j' way.
+
+   When emerging a more complex package depending on several
+   sub packages it is likely that emerge dies for some reasons 
+   at different parts of process. Without this tool you are
+   forced to fix the detected problem one after the other
+   and start the emerging again and again.
+
+   The 'emerge -k' does not stop at the first error and
+   builds as much as its possible to generate a maximal
+   error report.
+
+   In the dependency graph of an emerge target some parts
+   are independent from each other so it is possible to
+   build them parallel. This goes beyond a classical 'make -j'
+   because having this feature at emerge level you are
+   able get fetch sources of one package and compile another.
+
+   Usage:
+
+   Preconditon: Set the environment variables used by emerge.
+
+   $ python bin/parallel.py -j <# workers> -c <command>
+
+   with <# workers> is the number of parallel running jobs.
+   Defaults to the number of processor cores of the machine.
+
+   <command> is template for the command to be executed
+   for each sub package. Defaults to:
+   'python emerge.py %(category)s/%(package)s'
+
+   category, package, version and tag are subsituted
+   in the command string before being executed.
+"""
 
 __author__  = "Sascha L. Teichmann <sascha.teichmann@intevation.de>"
 __license__ = "GNU General Public License (GPL)"
@@ -10,7 +45,10 @@ import getopt
 import os.path
 import sys
 import traceback
+
 from datetime import datetime
+
+from multiprocessing import Process, Queue, cpu_count
 
 import portage
 
@@ -18,27 +56,39 @@ import utils
 
 from dependencies import DependenciesTree
 
-from multiprocessing import Process, Queue, cpu_count
 
 DEFAULT_COMMAND = "python %s %%(category)s/%%(package)s" % \
     os.path.join(os.getenv("KDEROOT", os.curdir), "bin", "emerge.py")
 
 
 def now():
+    """Returns current timestamp as string"""
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 def log(kind, msg):
+    """Writes timestamped message"""
     print "builder: %s %s %s" % (now(), kind, msg)
     sys.stdout.flush()
 
 class Job(object):
+    """An emerge work job.
+       References a node in the dependency graph to access the
+       informations to generate a concrete emerge call.
+       The number of blocking jobs is also tracked. If this
+       number goes down to zero this job is ready for execution.
+       Error codes are propagated through the calling jobs chains.
+    """
 
     def __init__(self, node):
+        """Creates a new emerge job for given node in the dependency graph."""
         self.node      = node
         self.dep_count = len(node.children)
         self.dep_exit  = 0
 
     def create_command(self, command):
+        """Fills the given command template with information from the
+           corresponding node in the dependency graph.
+        """
         node = self.node
         return command % {
             'category': node.category,
@@ -47,11 +97,18 @@ class Job(object):
             'tag'     : node.tag }
 
     def unblock(self, dep_exit):
+        """Decreases the count of blocking jobs which need to be
+           processed before this job is ready for execution.
+           Error codes are stored to be propagated through the
+           graph waiting for this job.
+           Returns true if this job is ready for execution.
+        """
         if dep_exit != 0: self.dep_exit = dep_exit
         self.dep_count -= 1
         return self.dep_count < 1
 
     def trigger_exec(self, command):
+        """Generates a tuple to enable the workers to execute this job."""
         if self.dep_exit != 0:
             # propagate error
             return (str(self.node), "", self.dep_exit)
@@ -59,8 +116,13 @@ class Job(object):
             return (str(self.node), self.create_command(command))
 
 class Worker(Process):
+    """An asynchronous working job executor.
+       It takes its jobs from a 'todo' queue
+       and puts the results to a 'done' queue.
+    """
 
     def __init__(self, todo, done):
+        """Creates a worker with a 'todo' and a 'done' job queue.""" 
         Process.__init__(self)
         self.todo   = todo
         self.done   = done
@@ -68,6 +130,9 @@ class Worker(Process):
         self.start()
 
     def run(self):
+        """Takes jobs from the 'todo' queue, processes them
+           and puts the results into the 'done' queue.
+        """
         while True:
             execute = self.todo.get()
             if len(execute) > 2:
@@ -86,11 +151,19 @@ class Worker(Process):
                 self.done.put((execute[0], exit_code))
 
 class ParallelBuilder(object):
+    """Converts a dependency graph into lists of jobs to be executed
+       in a botton up manner. Independent jobs are processed
+       in parallel with a given number of workers.
+    """
 
     def __init__(self, command):
+        """Creates a builder with a given command template."""
         self.command = command
 
     def build_blocked(self, node, blocked, jobs, ready):
+        """Recursive working method to create the jobs lists from the
+           dependency tree.
+        """
         key = str(node)
         try:
             return jobs[key]
@@ -111,6 +184,10 @@ class ParallelBuilder(object):
         return job
 
     def build(self, dep_tree, num_worker = None):
+        """Builds a dependency tree with a number of workers.
+           If no explicit number of workers is given the
+           number of processor cores is used.
+        """
 
         if num_worker is None: num_worker = cpu_count()
 
@@ -188,7 +265,6 @@ def main():
     builder = ParallelBuilder(command)
     
     log("start", "all")
-    print "parallel: %s start all" % now()
     exit_code = builder.build(dep_tree, num_worker)
     log("stop", "all")
 
