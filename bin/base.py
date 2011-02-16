KDE/emerge-history

commit d180ba01cdddf1cd36fc0a8891fb552364af715b

use PackageDestinationDir() also for baseclass and subclasses. Add parameter withBuildType
svn path=/trunk/kdesupport/emerge/; revision=1221083

bin/base.py
@@ -462,9 +462,7 @@ class baseclass:
         this requires the kdewin-packager"""
 
         # FIXME: add a test for the installer later
-        dstpath = os.getenv( "EMERGE_PKGDSTDIR" )
-        if not dstpath:
-            dstpath = os.path.join( self.rootdir, "tmp" )
+        dstpath = self.packageDestinationDir(withBuildType=False)
         binpath = os.path.join( self.imagedir, self.instdestdir )
         tmp = os.path.join( binpath, "kde" )
 
@@ -565,6 +563,24 @@ class baseclass:
         if not utils.system( command, *args, **kw ):
             utils.die( "os.system ( %s ) failed" % ( command ) )
         return True
+
+    def packageDestinationDir( self, withBuildType=True ):
+        """return absolute path to the directory where binary packages are placed into.
+        Default is to optionally append build type subdirectory"""
+
+        utils.debug( "base.packageDestinationDir called", 2 )
+        dstpath = os.getenv( "EMERGE_PKGDSTDIR" )
+        if not dstpath:
+            dstpath = os.path.join( self.rootdir, "tmp" )
+
+        if withBuildType:
+            if envAsBool( "EMERGE_MERGE_ROOT_WITH_BUILD_TYPE" ):
+                dstpath = os.path.join( dstpath, self.buildType())
+
+        if not os.path.exists(dstpath):
+            utils.createDir(dstpath)
+        return dstpath
+
 
 # ############################################################################################
 # for testing purpose only:

bin/EmergeBase.py
@@ -302,16 +302,18 @@ class EmergeBase(object):
             directory = ROOTDIR
         return self.__adjustPath(directory)
 
-    def packageDestinationDir( self ):
-        """return absolute path to the directory where binary packages are placed into"""
+    def packageDestinationDir( self, withBuildType=True ):
+        """return absolute path to the directory where binary packages are placed into.
+        Default is to optionally append build type subdirectory"""
 
         utils.debug( "EmergeBase.packageDestinationDir called", 2 )
         dstpath = os.getenv( "EMERGE_PKGDSTDIR" )
         if not dstpath:
             dstpath = os.path.join( self.rootdir, "tmp" )
 
-        if envAsBool( "EMERGE_MERGE_ROOT_WITH_BUILD_TYPE" ):
-            dstpath = os.path.join( dstpath, self.buildType())
+        if withBuildType:
+            if envAsBool( "EMERGE_MERGE_ROOT_WITH_BUILD_TYPE" ):
+                dstpath = os.path.join( dstpath, self.buildType())
 
         if not os.path.exists(dstpath):
             utils.createDir(dstpath)

bin/Packager/InnoSetupPackager.py
@@ -109,10 +109,7 @@ class InnoSetupPackager (PackagerBase):
                 filesDir = self.imageDir()
             utils.createManifestFiles(filesDir, filesDir, "", self.package, pkgVersion)
 
-        ## \todo do we have a wrapper for this ?
-        dstpath = os.getenv( "EMERGE_PKGDSTDIR" )
-        if not dstpath:
-            dstpath = os.path.join( self.rootdir, "tmp" )
+        dstpath = self.packageDestinationDir(withBuildType=False)
 
         cmd = "\"%s\" /O\"%s\" /F\"setup-%s-%s\"" % (self.packagerExe, dstpath, pkgName, pkgVersion)
 

portage/contributed/l10n-kde4-branch/l10n-kde4-branch-20080626.py
@@ -124,9 +124,7 @@ class subclass(base.baseclass):
 
     def make_package( self ):
         self.svnpath = os.path.join( self.kdesvndir, self.subinfo.svnTargets['svnHEAD'] )
-        dstpath = os.getenv( "EMERGE_PKGDSTDIR" )
-        if not dstpath:
-            dstpath = os.path.join( self.rootdir, "tmp" )
+        dstpath = self.packageDestinationDir(withBuildType=False)
 
         if not utils.test4application( "kdewin-packager" ):
             utils.die( "kdewin-packager not found - please make sure it is in your path" )

portage/enterprise5/l10n-kde4-e5/l10n-kde4-e5-20110117.py
@@ -155,9 +155,7 @@ class subclass(base.baseclass):
 
     def make_package( self ):
         self.svnpath = os.path.join( self.kdesvndir, self.subinfo.svnTargets['svnHEAD'] )
-        dstpath = os.getenv( "EMERGE_PKGDSTDIR" )
-        if not dstpath:
-            dstpath = os.path.join( self.rootdir, "tmp" )
+        dstpath = self.packageDestinationDir(withBuildType=False)
 
         if not utils.test4application( "kdewin-packager" ):
             utils.die( "kdewin-packager not found - please make sure it is in your path" )

portage/kde-4.1/l10n-kde4/l10n-kde4-20080320.py
@@ -120,9 +120,7 @@ class subclass(base.baseclass):
 
     def make_package( self ):
         self.svnpath = os.path.join( self.kdesvndir, self.subinfo.svnTargets['svnHEAD'] )
-        dstpath = os.getenv( "EMERGE_PKGDSTDIR" )
-        if not dstpath:
-            dstpath = os.path.join( self.rootdir, "tmp" )
+        dstpath = self.packageDestinationDir(withBuildType=False)
 
         if not utils.test4application( "kdewin-packager" ):
             utils.die( "kdewin-packager not found - please make sure it is in your path" )

portage/kde-4.2/l10n-kde4/l10n-kde4-20080320.py
@@ -114,9 +114,7 @@ class subclass(base.baseclass):
 
     def make_package( self ):
         self.svnpath = os.path.join( self.kdesvndir, self.subinfo.svnTargets['svnHEAD'] )
-        dstpath = os.getenv( "EMERGE_PKGDSTDIR" )
-        if not dstpath:
-            dstpath = os.path.join( self.rootdir, "tmp" )
+        dstpath = self.packageDestinationDir(withBuildType=False)
 
         if not utils.test4application( "kdewin-packager" ):
             utils.die( "kdewin-packager not found - please make sure it is in your path" )

commit 3db55b7b6a778d788bd3aefa536b209c84d91f95

regression in r1221083: envAsBool was unknown in base.py
this needs a better fix

svn path=/trunk/kdesupport/emerge/; revision=1221107

bin/base.py
@@ -53,6 +53,11 @@ EMERGE_MAKE_PROGRAM = os.getenv( "EMERGE_MAKE_PROGRAM" )
 # IMAGEDIR: the directory, under which the compiled files are installed.
 #            here rootdir/tmp/packagename/image
 
+def envAsBool(key):
+    """ return value of environment variable as bool value """
+    return os.getenv( key ) == "True" or os.getenv( key ) == "1"
+
+
 
 class baseclass:
     """